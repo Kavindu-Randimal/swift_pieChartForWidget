05ab421f14cddaa282072ec9e21e1987c0fe9a63
@randimalg <randimal@zorrosign.com>
2021-09-29 20:34:08 +0530

    first commit

---
+++ PieChartForWidget/PieChartForWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartForWidgetApp.swift
+//  PieChartForWidget
+//
+//  Created by Randimal Geeganage on 2021-06-29.
+//
+
+import SwiftUI
+
+@main
+struct PieChartForWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+          ContentView()
+        }
+    }
+}
